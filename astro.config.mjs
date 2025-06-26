a0751547bfe200e2e881335d64b787452190f991
Komeyl Kalhorinia <komylfa@gmail.com>
2025-06-26 20:40:32 +0330

    refactor: implement definitive deployment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 
-// This is the main configuration file for your Astro project.
+// https://astro.build/config
 export default defineConfig({
-  // The `site` property specifies the final, public URL of your website.
+  
   site: 'https://www.komyl.com',
-  base: '/',
 });
