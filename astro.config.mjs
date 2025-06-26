d757c3b150d1f817f8caa722ad2f9e0d816998d7
Komeyl Kalhorinia <komylfa@gmail.com>
2025-06-26 18:37:06 +0330

    config: set site URL for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // This is the most important setting for deployment
+  site: 'https://www.komyl.com',
+});
